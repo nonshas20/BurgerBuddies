cc5048de67f7f3eb931038d1a67173fc5ae9933f
Ramyuu <rainangelojmorcozo@iskolarngbayan.pup.edu.ph>
2023-05-18 11:44:45 +0800

    Added Logo

--- popo.Android/Resources/Resource.designer.cs
+++ popo.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace popo.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.93")]
 	public partial class Resource
 	{
 		
@@ -13252,6 +13252,9 @@ namespace popo.Droid
 			
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int logo = 2131492867;
 			
 			static Mipmap()
 			{
