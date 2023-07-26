300c8ca0f09eddde106b3864027d5e0e25686dea
Ramyuu <rainangelojmorcozo@iskolarngbayan.pup.edu.ph>
2023-07-26 22:31:55 +0800

    Can Add Product To Category
    -> Title says everything

--- popo/Views/Add item to category/AddItemsCategoryPage.xaml.cs
+++ popo/Views/Add item to category/AddItemsCategoryPage.xaml.cs
@@ -15,7 +15,50 @@ namespace popo
 
         private CategoryModel selectedCategory; //Declared selected category
 
+        async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string CategoryName = CategoryEntry.Text;
+            string ItemName = ItemNameEntry.Text;
+            string Price = PriceEntry.Text;
+            string Stocks = StocksEntry.Text;
+            if (string.IsNullOrWhiteSpace(ItemName))
+            {
+                await DisplayAlert("Invalid", "Enter Product Name!", "OK");
+            }
+            if (string.IsNullOrWhiteSpace(Price))
+            {
+                await DisplayAlert("Invalid", "Enter Product's Price!", "OK");
+            }
+            if (string.IsNullOrWhiteSpace(Stocks))
+            {
+                await DisplayAlert("Invalid", "Enter Product's Stock!", "OK");
+            }
 
+            else
+            {
+                AddProductToCategory();
+            }
+        }
+
+        async void AddProductToCategory()
+        {
+            if (int.TryParse(PriceEntry.Text, out int productCost) && int.TryParse(StocksEntry.Text, out int productStock))
+            {
+                await App.ProductsDatabase.CreateProducts(new ProductModel
+                {
+                    Category_Id = selectedCategory.Category_Id,
+                    Product_Name = ItemNameEntry.Text,
+                    Product_Cost = productCost,
+                    Product_Stock = productStock
+                });
+
+                await DisplayAlert("Success", "Product Added!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Invalid input for product cost or stock", "OK");
+            }
+        }
 
 
 
